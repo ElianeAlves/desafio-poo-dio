706ed21770f8400c33b7ac65d44d95e9744f01bb
Eliane <elianealves.melo96@gmail.com>
2022-07-29 17:40:13 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso Java 2022 pela DIO usando a IDE Intellij");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Springboot");
+        curso2.setDescricao("Curso Springboot 2022 pela DIO");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Mentoria Java ministrada pela DIO");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
